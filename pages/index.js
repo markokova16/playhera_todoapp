e1d08eaf2b50d147574c9c6ee571a9731c858ad1
markokova16 <markokovac1696@gmail.com>
2022-10-11 15:08:21 +0200

    Update index.js
    Everything works mostly as it should, a bit of refactoring needed but everything is functional without unexpected issues

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ const Index = () => {
 	const [userInput, setUserInput] = useState("");
 	const [todoList, setTodoList] = useState([]);
 	const [completedTodosIds, setCompletedTodosIds] = useState([]);
+	const [editState, setEditState] = useState(false);
+	const [editId, setEditId] = useState(null);
 
 	const handleChange = (e) => {
 		e.preventDefault();
@@ -49,46 +51,93 @@ const Index = () => {
 		setCompletedTodosIds([]);
 	};
 
+	const handleEdit = (todo, idx) => {
+		setEditState(true);
+		setEditId(idx);
+		setUserInput(todo);
+	};
+
+	const handleCancel = () => {
+		setEditState(false);
+		setEditId(null);
+		setUserInput("");
+	};
+
+	const handleSave = () => {
+		todoList[editId] = userInput;
+		setTodoList([...todoList]);
+		setEditState(false);
+		setEditId(null);
+		setUserInput("");
+	};
+
 	return (
-		<div className="flex flex-col  h-screen items-center ">
+		<div className="flex flex-col  h-screen items-center bg-black">
 			<h1 className="my-20   text-8xl font-extrabold text-red-700 ">
 				Next.js ToDo List
 			</h1>
-			<form>
+			<form className="flex">
 				<input
-					className="text-white text-center border-black border-10 bg-black ml-14 mr-5 w-[500px]"
+					className="text-black text-center border-black border-10 bg-white ml-14 mr-5 w-[500px]"
 					type="text"
 					onChange={handleChange}
 					value={userInput}
 					placeholder="Enter your task"
 				/>
-				<button
-					className="bg-red-300 text-black w-[70px] rounded-lg"
-					onClick={handleSubmit}
-					disabled={!userInput}
-				>
-					Submit
-				</button>
+
+				{editState ? (
+					<div className="flex">
+						<button
+							className="bg-white text-black w-[70px] rounded-lg px-5 mr-2"
+							type="button"
+							onClick={handleSave}
+						>
+							Save
+						</button>
+						<button
+							className="bg-white text-black w-[70px] rounded-lg "
+							onClick={handleCancel}
+							type="button"
+						>
+							Cancel
+						</button>
+					</div>
+				) : (
+					<button
+						className="bg-white text-black w-[70px] rounded-lg"
+						onClick={handleSubmit}
+						disabled={!userInput}
+					>
+						Submit
+					</button>
+				)}
 			</form>
 
-			<ul className="my-5 ">
+			<ul className="my-5 text-white ">
 				{todoList.length >= 1
 					? todoList.map((todo, idx) => {
 							return (
-								<li
-									onClick={(e) => handleClick(e, idx)}
-									key={idx}
-								>
+								<li className="text-white">
 									<span
+										onClick={(e) => handleClick(e, todo)}
+										key={todo}
 										className={
-											completedTodosIds.includes(idx) &&
+											completedTodosIds.includes(todo) &&
 											"line-through"
 										}
 									>
 										{todo}{" "}
 									</span>
+
 									<button
-										className="bg-black text-white w-[70px] my-5 ml-2 rounded-lg"
+										className="bg-white text-black w-[70px] my-5 ml-2 rounded-lg"
+										onClick={() => handleEdit(todo, idx)}
+									>
+										Edit
+									</button>
+
+									<button
+										className="bg-white text-black w-[70px] my-5 ml-2 rounded-lg"
 										onClick={(e) => {
 											e.preventDefault();
 											handleDelete(todo);
@@ -99,10 +148,10 @@ const Index = () => {
 								</li>
 							);
 					  })
-					: "What's your task?"}
+					: "What are your tasks for today? :)"}
 			</ul>
 			<button
-				className="bg-black text-white w-[200px]   rounded-lg"
+				className="bg-white text-black w-[200px]   rounded-lg"
 				onClick={deleteCompletedTodos}
 			>
 				Delete completed tasks
